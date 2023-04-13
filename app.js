eda05d521904bf720c1e3c6f76e78454edaf5433
ubaish Malik <ubaish9897@gmail.com>
2023-04-13 19:51:10 +0530

    fixed env variables

--- app.js
+++ app.js
@@ -31,7 +31,11 @@ app.use(cors({
 import otherRoutes from "./routes/Routes.js"
 
 app.get("/",(req,res)=>{
-    res.send(`Bckend working fine click <a href=${process.env.FRONTEND_URL}>Here</a> to go to frontend`)
+res.send("Working fine ");
+})
+
+app.get("/home",(req,res)=>{
+    res.send(` Click <a href=${process.env.FRONTEND_URL}>Here</a> to go to frontend`)
 })
 
 app.use("/api/v1",otherRoutes)
