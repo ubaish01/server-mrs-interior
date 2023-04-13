0b21c3cd772888ee7cc272eae9addf00dcf0461e
ubaish Malik <ubaish9897@gmail.com>
2023-04-13 20:10:36 +0530

    added vercel.json

---
+++ app.js
@@ -0,0 +1,45 @@
+import cors from 'cors'
+import cookieParser from 'cookie-parser';
+import express, { urlencoded } from 'express';
+import {config} from 'dotenv'
+import ErrorMiddleware from "./middlewares/Error.js"
+
+config({
+    path:'./config/config.env'
+});
+
+const app = express()
+
+//Using middlewares
+app.use(express.json());
+app.use(cookieParser());
+app.use(urlencoded(
+    {
+        extended:true
+    }
+));
+app.use(cors({
+    origin:process.env.FRONTEND_URL,
+    credentials:true,
+    methods:["GET","POST","PUT","DELETE"]
+}))
+
+
+
+
+//Imports and routes 
+import otherRoutes from "./routes/Routes.js"
+
+app.get("/",async(req,res)=>{
+res.send("Working fine ");
+})
+
+app.get("/home",(req,res)=>{
+    res.send(` Click <a href=${process.env.FRONTEND_URL}>Here</a> to go to frontend`)
+})
+
+app.use("/api/v1",otherRoutes)
+
+export default app
+
+app.use(ErrorMiddleware)
